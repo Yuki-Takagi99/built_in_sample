10fa44a026fbe736b11872bacf68a800a43fa9cd
yuki takagi <yuki99takagi@gmail.com>
2020-08-25 12:06:07 +0900

    Add: 4-3-2 Turbolinksによる画面遷移の高速化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener("turbolinks:load", function() {
+    console.log("turbolinks:load fire")
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
